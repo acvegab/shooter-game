b0cacf8e0dd731ee6fa2af42b1e85c0e5f96e04e
Carolina Vega <acvegab@gmail.com>
2018-01-18 20:01:33 -0600

    jump fixed

--- app/game-object.class.ts
+++ app/game-object.class.ts
@@ -4,15 +4,19 @@ export class GameObject implements IGameObject {
     public hp: number;
     public x: number;
     public y: number;
+    public originalY: number;
     public width: number;
     public height: number;
     public context: any;
     public color: string;
     public speed = 1;
+    public freeWill = false;
+    public jumping: any;
     constructor(x: number, y: number, w: number, h: number, color: string, ctx: any) {
         this.hp = 1;
         this.x = x;
         this.y = y;
+        this.originalY = this.y;
         this.width = w;
         this.height = h;
         this.context = ctx;
@@ -27,6 +31,9 @@ export class GameObject implements IGameObject {
         this.speed = speed;
         return this;
     }
+    public addFreeWill() {
+        this.freeWill = true;
+    }
 
     public moveFront() {
         this.x += (this.speed * 10);
@@ -35,6 +42,23 @@ export class GameObject implements IGameObject {
         this.x -= (this.speed * 10);
     }
     public jump() {
-        this.y -= 10;
+        let goingDown = false;
+        const me = this;
+        const limit = 150;
+        clearInterval(me.jumping);
+        this.jumping = setInterval(() => {
+            if (me.y > limit && !goingDown) {
+                me.y -= 3;
+                console.log('jumping: ' + me.y);
+            } else {
+                goingDown = true;
+                me.y += 3;
+                if (me.y > me.originalY) {
+                    clearInterval(me.jumping);
+                    me.y = me.originalY;
+                    goingDown = false;
+                }
+            }
+        }, 10);
     }
 }
